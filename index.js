5d6193d0917f32b243a6ef7ae5c830cb926183a8
Angel Jair Neri Huerta <yayodcv@gmail.com>
2024-11-18 12:09:49 -0600

    get url params

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import express from 'express';
 
 const app = express();
 const port = 3000;
+
 app.get('/', (req, res) => {
 	res.send('Express Server');
 });
@@ -10,13 +11,25 @@ app.get('/', (req, res) => {
 app.get('/nueva-ruta', (req, res) => {
 	res.send('Nueva ruta');
 });
+
 app.get('/products', (req, res) => {
-	res.json({
-		name: 'Product 1',
-		price: 1000,
-	});
+	res.json([
+		{ name: 'Product 1', price: 1000 },
+		{ name: 'Product 2', price: 4000 },
+	]);
+});
+
+app.get('/products/:id', (req, res) => {
+	const { id } = req.params;
+
+	res.json({ id, name: 'Product 2', price: 200 });
+});
+
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+	const { categoryId, productId } = req.params;
+	res.json({ categoryId, productId });
 });
 
 app.listen(port, () => {
-	console.log('esuchando en el puert' + port);
+	console.log('esuchando en el puerto' + port);
 });
