426936f1ce4ac3af5e03be62361a990cf1aace6a
Angel Jair Neri Huerta <yayodcv@gmail.com>
2024-11-18 14:31:54 -0600

    get query params

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // const express = require('express');
 import express from 'express';
+import { faker } from '@faker-js/faker';
 
 const app = express();
 const port = 3000;
@@ -13,16 +14,36 @@ app.get('/nueva-ruta', (req, res) => {
 });
 
 app.get('/products', (req, res) => {
-	res.json([
-		{ name: 'Product 1', price: 1000 },
-		{ name: 'Product 2', price: 4000 },
-	]);
+	const products = [];
+	const { size } = req.query;
+	const limit = size || 10;
+
+	for (let i = 0; i < limit; i++) {
+		products.push({
+			name: faker.commerce.productName(),
+			price: parseInt(faker.commerce.price(), 10),
+			image: faker.image.urlLoremFlickr(),
+		});
+	}
+
+	res.json(products);
+});
+
+app.get('/products/filter', (req, res) => {
+	res.json('soy filter');
 });
 
 app.get('/products/:id', (req, res) => {
 	const { id } = req.params;
-
 	res.json({ id, name: 'Product 2', price: 200 });
+});
+app.get('/users', (req, res) => {
+	const { limit, offset } = req.query;
+	if (limit && offset) {
+		res.json({ limit, offset });
+	} else {
+		res.send('No hay parametros');
+	}
 });
 
 app.get('/categories/:categoryId/products/:productId', (req, res) => {
