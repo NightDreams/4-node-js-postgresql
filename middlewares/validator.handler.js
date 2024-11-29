53d904a8fa43ccc97a9d5f4089109b2a1db712b1
Angel Jair Neri Huerta <yayodcv@gmail.com>
2024-11-29 16:30:05 -0600

    Implement  midleware validatorHandler and schemas for endpoints  in  prodcuts.router.

--- middlewares/validator.handler.js
+++ middlewares/validator.handler.js
@@ -1,18 +1,12 @@
 import boom from '@hapi/boom';
-function validatorHandler(schema, property) {
+export function validatorHandler(schema, property) {
 	return (req, res, next) => {
 		const data = req[property];
-		const { error } = schema.validate(data);
+		// abortEarly - send all errors
+		const { error } = schema.validate(data, { abortEarly: false });
 		if (error) {
-			next (boom.badRequest(error););
-		} else{
-			next();
+			next(boom.badRequest(error));
 		}
+		next();
 	};
-
-	res.status(500).json({
-		message: err.message,
-		stack: err.stack,
-	});
 }
-export default validatorHandler;
--- routes/products.router.js
+++ routes/products.router.js
@@ -2,6 +2,13 @@ import express from 'express';
 import { faker } from '@faker-js/faker';
 import ProductsService from '../services/product.service.js';
 
+import { validatorHandler } from '../middlewares/validator.handler.js';
+import {
+	createProductSchema,
+	getProductSchema,
+	updateProductSchema,
+} from '../schema/product.schema.js';
+
 const router = express.Router();
 const service = new ProductsService();
 
@@ -14,33 +21,52 @@ router.get('/filter', (req, res) => {
 	res.json('soy filter');
 });
 
-router.get('/:id', async (req, res, next) => {
-	try {
-		const { id } = req.params;
-		const product = await service.findOne(id);
-		res.status(201).json(product);
-	} catch (error) {
-		// run middlewar for errors
-		next(error);
+router.get(
+	'/:id',
+	//concatenar middwere
+	// indicamos manualmete a donde extrae los datos.
+	validatorHandler(getProductSchema, 'params'),
+
+	// esto es un midl"conect service ".	podemos concatenar
+	async (req, res, next) => {
+		try {
+			const { id } = req.params;
+			const product = await service.findOne(id);
+			res.status(201).json(product);
+		} catch (error) {
+			// run middlewar for errors
+			next(error);
+		}
 	}
-});
+);
 
-router.post('/', async (req, res) => {
-	const body = req.body;
-	const newProduct = await service.create(body);
-	res.status(201).json(newProduct);
-});
-
-router.patch('/:id', async (req, res, next) => {
-	try {
-		const { id } = req.params;
+router.post(
+	'/',
+	validatorHandler(createProductSchema, 'body'),
+	async (req, res) => {
 		const body = req.body;
-		const product = await service.update(id, body);
-		res.json(product);
-	} catch (error) {
-		next(error);
+		const newProduct = await service.create(body);
+		res.status(201).json(newProduct);
 	}
-});
+);
+
+router.patch(
+	'/:id',
+	//procesar id
+	validatorHandler(getProductSchema, 'params'),
+	//procesar update . Son secuenciales
+	validatorHandler(updateProductSchema, 'body'),
+	async (req, res, next) => {
+		try {
+			const { id } = req.params;
+			const body = req.body;
+			const product = await service.update(id, body);
+			res.json(product);
+		} catch (error) {
+			next(error);
+		}
+	}
+);
 
 router.delete('/:id', async (req, res) => {
 	const { id } = req.params;
