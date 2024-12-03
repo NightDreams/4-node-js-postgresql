6ea8a622adcae92ee1139d539caf273a84f1253b
Angel Jair Neri Huerta <yayodcv@gmail.com>
2024-12-03 15:11:28 -0600

    Setup for heroku deploy

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ import {
 } from './middlewares/error.handler.js';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 // midleware for json
 app.use(express.json());
 
